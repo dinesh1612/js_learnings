35cf2164aeb66370fae163d91afaea2f3945da91
dinesh reddy <dinesh.reddy@fraazo.com>
2022-02-15 16:40:43 +0530

    dinesh

---
+++ dinesh.js
@@ -0,0 +1,16 @@
+alert("dinesh reddy"); // popup alert
+prompt("what is your name?") // take input from user
+typeof(123) // var type number
+typeof("dinesh") // var type string
+typeof(true) // var type boolean
+var myName = "dinesh Reddy"; // defying variable
+myName.length; // lenghth of string
+myName.slice(0,1); //slice a string first number is valid second number is not used it implies range
+myName.toLowerCase(); //to lower case
+myName.toUpperCase(); // to upper case
+// in java script cariable name starts with small letter and then for every new word captial letter is the indicative
+function myFunction() {
+    // write you code here // basic syntax of function // defyining the function
+}
+myFunction(); // this is how function is called
+console.log(myName) // this shows the value in console

824f058094454f65687c729c4fd43a7dda8257e1
dinesh reddy <dinesh.reddy@fraazo.com>
2022-02-21 07:55:13 +0530

    update

--- learnings.js
+++ learnings.js
@@ -1,3 +1,4 @@
+// js is an intrepreted programming langugae, code is executed line by line
 alert("dinesh reddy"); // popup alert
 prompt("what is your name?") // take input from user
 typeof(123) // var type number
@@ -8,7 +9,7 @@ myName.length; // lenghth of string
 myName.slice(0,1); //slice a string first number is valid second number is not used it implies range
 myName.toLowerCase(); //to lower case
 myName.toUpperCase(); // to upper case
-// in java script cariable name starts with small letter and then for every new word captial letter is the indicative
+// in java script variable name starts with small letter and then for every new word captial letter is the indicative
 function myFunction() {
     // write you code here // basic syntax of function // defyining the function
 }
@@ -22,4 +23,19 @@ var intvalue = Math.round( floatvalue ); // basic math rounding style
 var intvalue = Math.trunc( floatvalue ); // it gives the integer value of the decimal number and eliminates the decimal part
 Math.pow(base, power) // to calculate power
 Math.random(); // generates random 16 digit decimal value from 0 to 1 but 1 not inclusive
-// if we want to have a random of range (1,6) or (1, 100) or any range starting from 1 multiply the random number generated with the max value in range Math.random();* (6 or 100 as you wish to do so)
+// if we want to have a random of range (1,6) or (1, 100) or any range starting from 1 multiply the random number generated with the max value in range 
+// Math.random();* (6 or 100 as you wish to do so)
+// if esle conditions in js
+if (/* condition */){
+    // write some code if if is true
+}
+else {
+    // write some code if if is false
+}
+// in js there is a difference between == and === 
+// when using == is only checks the var (eg 1 == "1" returns true) where as (1 === "1" return false)
+// == checks only the value, where as === checks value and data type of the value as well
+var array = ["dinesh", "reddy", "reddem"] // define array
+array.includes("dinesh") // returns true or false
+array.push("satyam") // satyam string is added to the array 
+array.pop(); // it removes the last element in array

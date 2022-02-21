6b99b0780e7267dc2f4f74b9435a99b0947a3cae
dinesh reddy <dinesh.reddy@fraazo.com>
2022-02-21 06:05:37 +0530

    update

--- learnings.js
+++ learnings.js
@@ -13,11 +13,13 @@ function myFunction() {
     // write you code here // basic syntax of function // defyining the function
 }
 myFunction(); // this is how function is called
-var output = myFunction();//we can use return in side the function to give an output and save it in an variable
-console.log(myName); // this shows the value in console
+var output = myFunction();//we can use return inside the function to give an output and save it in an variable
+console.log(myName); // this shows the value of myName variable in console
 var floatvalue = 10.23564
 var intvalue = Math.floor( floatvalue ); // 10
 var intvalue = Math.ceil( floatvalue ); // 11
 var intvalue = Math.round( floatvalue ); // basic math rounding style
-var intvalue = Math.trunc( floatvalue ); // 10 same as floor
+var intvalue = Math.trunc( floatvalue ); // it gives the integer value of the decimal number and eliminates the decimal part
 Math.pow(base, power) // to calculate power
+Math.random(); // generates random 16 digit decimal value from 0 to 1 but 1 not inclusive
+// if we want to have a random of range (1,6) or (1, 100) or any range starting from 1 multiply the random number generated with the max value in range Math.random();* (6 or 100 as you wish to do so)
